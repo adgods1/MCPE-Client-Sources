3ebbc6072ee19c5c8097531c6b91384ff7cfdf3d
adgods1 <98714456+adgods1@users.noreply.github.com>
2022-04-16 20:25:05 +0100

    Delete Da Drip Source 1.16.201 directory

--- Da Drip Source 1.16.201/DaDrip/Module/Modules/NotoSans.cpp
+++
@@ -1,9 +0,0 @@
-#include "NotoSans.h"
-NotoSans::NotoSans() : IModule(0, Category::VISUAL, "NotoSans") {
-}
-
-NotoSans::~NotoSans() {
-}
-const char* NotoSans::getModuleName() {
-	return "NotoSans";
-}
