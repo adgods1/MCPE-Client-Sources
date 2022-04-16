53da95efdc66737378606928be444bb2b8e2085e
adgods1 <98714456+adgods1@users.noreply.github.com>
2022-04-16 20:11:55 +0100

    Delete Badman directory

--- Badman/Client/BadMan/Module/Modules/LogMessage.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Module.h"
-class Logs : public IModule {
-public:
-	Logs();
-	~Logs();
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-};
--- Badman/Client/BadMan/Module/Modules/NoMiningTrace.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Module.h"
-class NoMiningTrace : public IModule {
-public:
-	NoMiningTrace();
-	~NoMiningTrace();
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-};
